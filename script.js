fffe46771cbf554ff710006f7e3af60752dba6a1
Abhilash Kumar Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2026-05-15 23:44:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,22 +5,7 @@ let p=document.querySelector('p');
 btns.forEach((btn)=>{
 	btn.addEventListener("click",(e)=>{
 
-		if(e.target.textContent=="applause"){
-			audio.play("applause.mp3")
-		}else if(e.target.textContent=="boo"){
-			audio.play("boo.mp3")
-		}else if(e.target.textContent=="gasp"){
-			audio.play("gasp.mp3")
-		}else if(e.target.textContent=="tada"){
-			audio.play("tada.mp3")
-		}else if(e.target.textContent=="victory"){
-			audio.play("victory.mp3")
-		}else if(e.target.textContent=="wrong"){
-			audio.play("wrong.mp3")
-		}else if(e.target.textContent=="applause"){
-			audio.play("")
-		}
-		
-	p.textContent=e.target.textContent;
+			const audio=new Audio(`sounds/${e.target.textContent}.mp3`)
+			audio.play();
 })
 })
